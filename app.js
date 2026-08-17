651df76df48976f6d0a3a3ae257cefac67ded87e
David <davidaubin02@gmail.com>
2026-08-17 11:35:09 +0200

    mise en place serveur + routes

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const mongoose = require ('mongoose');
+
+const Book=require('./Models/book');
+const bookRoutes = require('./Routes/book');
+const { config } = require('dotenv');
+
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((error) => console.log('Connexion à MongoDB échouée !', error));
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+app.use('api/book', bookRoutes);
+module.exports = app;
